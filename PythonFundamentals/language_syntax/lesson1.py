08e7838efcfb1b73eb372b26a5cae3cff6eabb09
Asadbek Otabekov <otabekovasadbek6@gmail.com>
2026-05-03 20:38:56 +0900

    Add initial Python syntax examples and output demonstrations

---
+++ PythonFundamentals/language_syntax/lesson1.py
@@ -0,0 +1,12 @@
+import sys
+## Indentation vs Braces 
+if 5 > 2: 
+    print("Five is greater than two!")
+## Basic Data Types
+print("Hello World")
+
+# showing the Python version
+print(sys.version)
+
+# Print "Have a good day!"
+print("Have a good day!")
---
+++ PythonFundamentals/language_syntax/outputs.py
@@ -0,0 +1,16 @@
+print("Hello World!", end=" ")
+print("I will print on the same line.")
+
+
+print(3)
+print(354)
+print(590000)
+
+
+print(3 + 2)
+print(3 - 2)
+print(3 * 2)
+print(3 / 2)
+print("I am", 35, "years old.")
+
+print("I am " , 25)
